a34700f5e11d67e900c22e6a81c7af2d0e00438a
Aria7121 <Aria.sarfaraz@gmail.com>
2025-06-16 13:09:07 +0300

    Changed models.py to use sqlLite insted of PostGreSql just for testing purposes

--- ada/models.py
+++ ada/models.py
@@ -1,30 +1,37 @@
 import os
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Date, Time, DateTime
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.dialects.postgresql import JSON 
+#sql lite (Testing)
+from sqlalchemy.sql import func
 # API Keys 
 from dotenv import load_dotenv
 load_dotenv()
 
+# SQLite engine (no username/password or .env needed)
+engine = create_engine("sqlite:///local.db", echo=True)
 
-# ✅ Get environment variables using os.environ.get
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
-
-
-url = URL.create(
-    drivername="postgresql",
-    username=db_user,
-    password=db_password,
-    host="localhost",
-    database="adademo",
-    port=5432
-)
-
-engine = create_engine(url)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+# ✅ Get environment variables using os.environ.get
+#db_user = os.environ.get("DB_USER")
+#db_password = os.environ.get("DB_PASSWORD")
+
+
+# url = URL.create(
+#     drivername="postgresql",
+#     username=db_user,
+#     password=db_password,
+#     host="localhost",
+#     database="adademo",
+#     port=5432
+# )
+
+# engine = create_engine(url)
+# SessionLocal = sessionmaker(bind=engine)
+# Base = declarative_base()
 
 class Conversation(Base):
     __tablename__ = "new_reservations"
@@ -33,10 +40,12 @@ class Conversation(Base):
     sender = Column(String)
     name = Column(String)
     surname = Column(String)
-    date = Column(String)
-    time = Column(String)
+    date = Column(String) #switch to Date
+    time = Column(String) # You can later switch to Time
     reservation_type = Column(String)
     party_size = Column(String)
+    status = Column(String, default="confirmed")
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 
 Base.metadata.create_all(engine)
